dad7c48f9bb7a1d822ab510ef3accbedd4fac6b0
dyaksa <diasnour0395@gmail.com>
2019-11-01 18:55:01 +0700

    connect database

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const slugify = require("slugify");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 const _ = require("lodash");
 const text = require(__dirname + "/library.js");
 
 const aboutHome = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, distinctio. Laudantium doloribus, culpa velit fugiat veritatis, nihil quasi inventore tenetur adipisci quia facere sunt explicabo laboriosam, minima architecto dignissimos. Quo.";
 const indexHome = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, distinctio. Laudantium doloribus, culpa velit fugiat veritatis, nihil quasi inventore tenetur adipisci quia facere sunt explicabo laboriosam, minima architecto dignissimos. Quo.";
 const contactHome = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, distinctio. Laudantium doloribus, culpa velit fugiat veritatis, nihil quasi inventore tenetur adipisci quia facere sunt explicabo laboriosam, minima architecto dignissimos. Quo."
+
+mongoose.connect("mongodb+srv://admin_dyaksa:tuesblues_030195@cluster0-pnlxn.mongodb.net/dailyjournalDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(function (value) {
+    console.log("success connect");
+}).catch(function (err) {
+    console.log("not connected databse");
+    throw err;
+});
+
+const postSchema = Schema({
+    _id: mongoose.Types.ObjectId,
+    title: {
+        type: String,
+        maxlength: 100
+    },
+    slug: String,
+    content: {
+        type: String,
+        minglength: 100
+    }
+});
+
+const Posts = mongoose.model("Post", postSchema);
 
 const app = express();
 app.use(express.static("public"));
@@ -18,9 +45,12 @@ app.set("view engine", "ejs");
 const posts = [];
 
 app.get("/", function (req, res) {
-    res.render("index", {
-        title: indexHome,
-        posts: posts
+    Posts.find({}, function (err, posts) {
+        if (err) throw err;
+        res.render("index", {
+            title: indexHome,
+            posts: posts
+        });
     });
 });
 
@@ -43,28 +73,36 @@ app.get("/compose", function (req, res) {
 app.post("/compose", function (req, res) {
     let title = req.body.title;
     let content = req.body.content;
-    const post = {
-        slug: slugify(_.lowerCase(title)),
-        title: _.upperFirst(title),
+    const post = new Posts({
+        _id: new mongoose.Types.ObjectId(),
+        title: title,
+        slug: slugify(_.toLower(title)),
         content: content
-    };
-    posts.push(post);
-    res.redirect("/");
+    });
+
+    post.save(function (err) {
+        if (err) throw err;
+        res.redirect("/");
+    });
 });
 
-app.get("/posts/:postName", function (req, res) {
-    const title = req.params.postName;
-    posts.forEach(function (post) {
-        if (post.slug === slugify(_.lowerCase(title))) {
-            let detailPost = {
-                title: post.title,
-                content: post.content
-            };
-            res.render("post", detailPost);
+app.get("/posts/:title/:id", function (req, res) {
+    const title = req.params.title;
+    const id = req.params.id;
+
+    Posts.findOne({
+        _id: id,
+        slug: title
+    }, function (err, post) {
+        if (err) throw err;
+        if (!post) {
+            console.log("tidak ditemukan");
         } else {
-            console.log("not Match");
+            res.render("post", {
+                post: post
+            });
         }
     });
 });
 
-app.listen(3000, () => console.log("port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("port 3000"));
